a906e2165e55d7d6f9ce585a0b00a3972f0c4745
sozysozbot <sozysozbot@gmail.com>
2017-11-09 16:23:14 +0900

    リパーシェで表示できるように

--- decompose_gui.js
+++ decompose_gui.js
@@ -14,12 +14,12 @@ if(!document.getElementById("json").checked){
 	var text = "";
 
 	for(var j=0; j<words.length; j++) {
-		text +=  "<br>" + words[j] + ":";
+		text +=  "<br><span class='toggle'>" + words[j] + "</span>:";
 		text += "<table>";
 		var result = decompose(words[j]);
 		
 		for(var i=0; i<result.length; i++){
-			text += "<tr><td>" + JSON.stringify(result[i]) + "</td></tr>";
+			text += "<tr><td class='toggle'>" + JSON.stringify(result[i]) + "</td></tr>";
 		}
 		text += "</table>";
 	}
@@ -30,3 +30,13 @@ if(!document.getElementById("json").checked){
 		= "<textarea>" + JSON.stringify(FISTIR) + "</textarea>"
 }
 }
+
+function toggle(isLiparxe)
+{
+	var arr = document.getElementsByClassName("toggle");
+	for(var i=0; i<arr.length; i++) {
+		arr[i].style.fontFamily = isLiparxe ? "Cirlipa" : "none";
+	}
+}
+
+
