54838e9e17ebfe472e6fda9be37c1f5064af277c
hsjoihs <hs.ioling.hs@gmail.com>
2019-05-31 19:47:08 +0900

    start migrating to TypeScript

--- decompose.js
+++ decompose.js
@@ -1,54 +1,47 @@
-function decompose(text)
-{
-	return decompose_(text, "INITIAL");
+function decompose(text) {
+    return decompose_(text, "INITIAL");
 }
-
-function getW()
-{
-	if(document.getElementById("obsolete").checked){
-		return W_OLD;
-	} else {
-		return W;
-	}
+function getW() {
+    if (document.getElementById("obsolete").checked) {
+        return W_OLD;
+    }
+    else {
+        return W;
+    }
 }
-
 /*
  * parse, from left, the possible parsing that can come after the previousState
  * if impossible, returns an empty array
  * decompose_("leter", "q-")
  *
  */
-function decompose_(text, previousState)
-{
-	var possibleStates = getW().rules
-		.filter(function(arr){return arr && arr[0] == previousState})
-		.map(function(arr){return arr[1]});
-
-	if(text === "") {
-		if(possibleStates.includes("FINAL")) {
-			return [[]]; // one possibility
-		} else {
-			return []; // zero possibility
-		}
-	}
-
-	var ans = [];
-	for(var i=0; i<possibleStates.length; i++) {
-		if(possibleStates[i] === "FINAL"){ continue; }
-
-		var candidate = getW().words[possibleStates[i]].filter(function(c){
-			return text.startsWith(c.replace(/-/g,""))
-		});
-
-		for(var j=0; j<candidate.length; j++){
-			var newText = text.slice(candidate[j].replace(/-/g,"").length);
-			var results = decompose_(newText, possibleStates[i]).map(function(res){
-				return [candidate[j]].concat(res);
-			});
-			ans = ans.concat(results);
-		}
-	}
-	return ans;
+function decompose_(text, previousState) {
+    var possibleStates = getW().rules
+        .filter(function (arr) { return arr && arr[0] == previousState; })
+        .map(function (arr) { return arr[1]; });
+    if (text === "") {
+        if (possibleStates.includes("FINAL")) {
+            return [[]]; // one possibility
+        }
+        else {
+            return []; // zero possibility
+        }
+    }
+    var ans = [];
+    for (var i = 0; i < possibleStates.length; i++) {
+        if (possibleStates[i] === "FINAL") {
+            continue;
+        }
+        var candidate = getW().words[possibleStates[i]].filter(function (c) {
+            return text.startsWith(c.replace(/-/g, ""));
+        });
+        for (var j = 0; j < candidate.length; j++) {
+            var newText = text.slice(candidate[j].replace(/-/g, "").length);
+            var results = decompose_(newText, possibleStates[i]).map(function (res) {
+                return [candidate[j]].concat(res);
+            });
+            ans = ans.concat(results);
+        }
+    }
+    return ans;
 }
-
-
--- decompose_gui.js
+++ decompose_gui.js
@@ -1,62 +1,54 @@
-function split(txt)
-{
-	return txt.toLowerCase()
-		.replace(/[,\.\?!<>":【】=…()\/―]/g," ")
-		.replace(/\s+/g," ").split(" ")
-		.filter(function(a){return a.length>0;});
+function split(txt) {
+    return txt.toLowerCase()
+        .replace(/[,\.\?!<>":【】=…()\/―]/g, " ")
+        .replace(/\s+/g, " ").split(" ")
+        .filter(function (a) { return a.length > 0; });
 }
 var FISTIR = [];
-function gui()
-{
-	var words = split(document.getElementById('word').value);
-	FISTIR = words.map(u => [u, decompose(u)]);
-if(!document.getElementById("json").checked){
-	var text = "";
-
-	for(var j=0; j<words.length; j++) {
-		text +=  "<br><span class='toggle'>" + escapeHTML(words[j]) + "</span>:";
-		text += "<table>";
-		var result = decompose(words[j]);
-		
-		for(var i=0; i<result.length; i++){
-			text += "<tr><td class='toggle'>";
-			for(var k=0; k<result[i].length; k++) {
-				if(k != 0){
-					text += ", ";
-				}
-				var url = "http://twoc.ever.jp/twoc/conlang.cgi?search=" + encodeURIComponent(result[i][k]) + "&type=0&agree=0&mode=search&user_id=fafs";
-				text +="<a href='" + escapeHTML(url) + "' target='_blank'>" + escapeHTML(result[i][k]) + "</a>";
-			}
-			text += "</td></tr>";
-		}
-		text += "</table>";
-	}
-	
-	document.getElementById("ans").innerHTML = text;
-	toggle(document.getElementById("toggler").checked);
-} else {
-	document.getElementById("ans").innerHTML 
-		= "<textarea>" + JSON.stringify(FISTIR) + "</textarea>"
+function gui() {
+    var words = split(document.getElementById('word').value);
+    FISTIR = words.map(u => [u, decompose(u)]);
+    if (!document.getElementById("json").checked) {
+        var text = "";
+        for (var j = 0; j < words.length; j++) {
+            text += "<br><span class='toggle'>" + escapeHTML(words[j]) + "</span>:";
+            text += "<table>";
+            var result = decompose(words[j]);
+            for (var i = 0; i < result.length; i++) {
+                text += "<tr><td class='toggle'>";
+                for (var k = 0; k < result[i].length; k++) {
+                    if (k != 0) {
+                        text += ", ";
+                    }
+                    var url = "http://twoc.ever.jp/twoc/conlang.cgi?search=" + encodeURIComponent(result[i][k]) + "&type=0&agree=0&mode=search&user_id=fafs";
+                    text += "<a href='" + escapeHTML(url) + "' target='_blank'>" + escapeHTML(result[i][k]) + "</a>";
+                }
+                text += "</td></tr>";
+            }
+            text += "</table>";
+        }
+        document.getElementById("ans").innerHTML = text;
+        toggle(document.getElementById("toggler").checked);
+    }
+    else {
+        document.getElementById("ans").innerHTML
+            = "<textarea>" + JSON.stringify(FISTIR) + "</textarea>";
+    }
 }
+function toggle(isLiparxe) {
+    var arr = document.getElementsByClassName("toggle");
+    for (var i = 0; i < arr.length; i++) {
+        arr[i].style.fontFamily = isLiparxe ? "Cirlipa" : "none";
+    }
 }
-
-function toggle(isLiparxe)
-{
-	var arr = document.getElementsByClassName("toggle");
-	for(var i=0; i<arr.length; i++) {
-		arr[i].style.fontFamily = isLiparxe ? "Cirlipa" : "none";
-	}
-}
-
 function escapeHTML(string) {
-	return string.replace(/&/g, "&amp;").replace(/'/g, "&#039;");
+    return string.replace(/&/g, "&amp;").replace(/'/g, "&#039;");
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-	document.getElementById("split").addEventListener("click", gui);
-	document.getElementById("word").addEventListener("keypress", function(e) {
-		if (e.keyCode == 13) {
-			gui();
-		}
-	});
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("split").addEventListener("click", gui);
+    document.getElementById("word").addEventListener("keypress", function (e) {
+        if (e.keyCode == 13) {
+            gui();
+        }
+    });
 });
