1b0260b2edeb9fbe98ea04b229f805405d8a444d
hsjoihs <hs.ioling.hs@gmail.com>
2019-06-01 04:38:59 +0900

    tsfmt

--- typescript/decompose.ts
+++ typescript/decompose.ts
@@ -1,15 +1,13 @@
-function decompose(text: string)
-{
-	return decompose_(text, "INITIAL");
+function decompose(text: string) {
+    return decompose_(text, "INITIAL");
 }
 
-function getW()
-{
-	if((document.getElementById("obsolete") as HTMLInputElement).checked){
-		return W_OLD;
-	} else {
-		return W;
-	}
+function getW() {
+    if ((document.getElementById("obsolete") as HTMLInputElement).checked) {
+        return W_OLD;
+    } else {
+        return W;
+    }
 }
 
 /*
@@ -18,38 +16,37 @@ function getW()
  * decompose_("leter", "q-") --> ["-l-","et","-er"]
  *
  */
-function decompose_(text: string, previousState: state): string[][]
-{
-	var possibleStates: state_noninitial[] = getW().rules
-		.filter(function(arr){return arr && arr[0] == previousState})
-		.map(function(arr){return arr[1]});
+function decompose_(text: string, previousState: state): string[][] {
+    var possibleStates: state_noninitial[] = getW().rules
+        .filter(function(arr) { return arr && arr[0] == previousState })
+        .map(function(arr) { return arr[1] });
 
-	if(text === "") {
-		if(possibleStates.includes("FINAL")) {
-			return [[]]; // one possibility
-		} else {
-			return []; // zero possibility
-		}
-	}
+    if (text === "") {
+        if (possibleStates.includes("FINAL")) {
+            return [[]]; // one possibility
+        } else {
+            return []; // zero possibility
+        }
+    }
 
-	var ans: string[][] = [];
-	for(var i=0; i<possibleStates.length; i++) {
-		const state = possibleStates[i];
-		if(state === "FINAL"){ continue; }
+    var ans: string[][] = [];
+    for (var i = 0; i < possibleStates.length; i++) {
+        const state = possibleStates[i];
+        if (state === "FINAL") { continue; }
 
-		var candidate: string[] = getW().words[state].filter(function(c: string){
-			return text.startsWith(c.replace(/-/g,""))
-		});
+        var candidate: string[] = getW().words[state].filter(function(c: string) {
+            return text.startsWith(c.replace(/-/g, ""))
+        });
 
-		for(var j=0; j<candidate.length; j++){
-			var newText = text.slice(candidate[j].replace(/-/g,"").length);
-			var results = decompose_(newText, possibleStates[i]).map(function(res){
-				return [candidate[j]].concat(res);
-			});
-			ans = ans.concat(results);
-		}
-	}
-	return ans;
+        for (var j = 0; j < candidate.length; j++) {
+            var newText = text.slice(candidate[j].replace(/-/g, "").length);
+            var results = decompose_(newText, possibleStates[i]).map(function(res) {
+                return [candidate[j]].concat(res);
+            });
+            ans = ans.concat(results);
+        }
+    }
+    return ans;
 }
 
 
