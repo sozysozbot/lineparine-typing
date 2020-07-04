a35d479399e2c61296c99abfb8f18e261581be80
skytomo <tomo1kawa.fullerene@gmail.com>
2020-07-04 17:20:18 +0900

    Add isMatchToClass

--- typescript/decompose.ts
+++ typescript/decompose.ts
@@ -1,5 +1,5 @@
 function decompose(text: string) {
-    return decompose_(text, "INITIAL");
+    return decompose_(text, "INITIAL", '');
 }
 
 function getW() {
@@ -16,10 +16,10 @@ function getW() {
  * decompose_("leter", "q-") --> ["-l-","et","-er"]
  *
  */
-function decompose_(text: string, previousState: state): string[][] {
+function decompose_(text: string, previousState: state, previousText: string): string[][] {
     const possibleStates: state_noninitial[] = getW().rules
-        .filter(function(arr) { return arr && arr[0] == previousState })
-        .map(function(arr) { return arr[1] });
+        .filter(function (arr) { return arr && arr[0] == previousState })
+        .map(function (arr) { return arr[1] });
 
     if (text === "") {
         if (possibleStates.includes("FINAL")) {
@@ -34,13 +34,14 @@ function decompose_(text: string, previousState: state): string[][] {
         const state = possibleStates[i];
         if (state === "FINAL") { continue; }
 
-        const candidate: string[] = getW().words[state].filter(function(c: string) {
+        const candidate: string[] = getW().words[state].filter(function (c: string) {
             return text.startsWith(c.replace(/-/g, ""))
         });
 
         for (let j = 0; j < candidate.length; j++) {
+            if ((state == '-q1-' || state == '-q2-') && !isMatchToClass(previousText, candidate[j])) { continue; }
             const newText = text.slice(candidate[j].replace(/-/g, "").length);
-            const results = decompose_(newText, possibleStates[i]).map(function(res) {
+            const results = decompose_(newText, possibleStates[i], previousText + candidate[j]).map(function (res) {
                 return [candidate[j]].concat(res);
             });
             ans = ans.concat(results);
@@ -49,4 +50,32 @@ function decompose_(text: string, previousState: state): string[][] {
     return ans;
 }
 
+function lastLetter(word: string) {
+    const letters: string[] = [
+        "fh", "vh", "dz", "ph", "ts", "ch", "ng", "sh", "th", "dh", "kh", "rkh", "rl",
+        "i", "y", "u", "o", "e", "a",
+        "p", "f", "t", "c", "x", "k", "q", "h", "r", "z", "m", "n", "r", "l", "j", "w", "b", "v", "d", "s", "g",
+    ];
+    for (const letter in letters) {
+        if (word.replace('-', '').replace("'", '').endsWith(letter)) {
+            return letter;
+        }
+    }
+    return '';
+}
 
+function lastVowelLetter(word: string) {
+    return lastLetter(word.replace(/[^iyuoea]/g, ''));
+}
+
+function isMatchToClass(left: string, right: string) {
+    const classMap: { [index: string]: string[]; } = {
+        ['a']: ["-a-", "-v-",],
+        ['o']: ["-a-", "-v-",],
+        ['e']: ["-e-", "-rg-",],
+        ['i']: ["-e-", "-rg-",],
+        ['u']: ["-u-", "-m-",],
+        ['y']: ["-i-", "-l-",],
+    };
+    return classMap[lastVowelLetter(left)].includes(right);
+}
